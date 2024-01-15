8d0987a08d01f76710270897c5994870b8ad78fc
muazahmd <63445732+muazahmd@users.noreply.github.com>
2024-01-15 13:54:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 9000;
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
